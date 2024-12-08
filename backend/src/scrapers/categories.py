dd8061b5a7c93711a39b7f68179c6f4bfda4e40d
mfaks <muhammad.faks@gmail.com>
2024-12-08 11:17:28 -0800

    feat: added categories

---
+++ backend/src/scrapers/categories.py
@@ -0,0 +1,33 @@
+class Categories:
+    @staticmethod
+    def get_categories():
+        return {
+            'Backend and Infrastructure': [
+                'backend', 'infrastructure', 'server', 'database', 'api', 
+                'microservices', 'architecture', 'cloud infrastructure', 
+                'kubernetes', 'docker', 'devops', 'scalability', 'performance'
+            ],
+            'Artificial Intelligence and Machine Learning': [
+                'artificial intelligence', 'machine learning', 'machine', 'neural network', 
+                'deep learning', 'ai', 'natural language processing', 
+                'computer vision', 'generative ai', 'predictive analytics', 
+                'reinforcement learning', 'data science', 'predictive', 'series'
+            ],
+            'Mobile and Web Development': [
+                'mobile', 'web', 'frontend', 'full stack', 
+                'javascript', 'react', 'angular', 'vue', 'html', 'css'
+            ],
+            'Cloud Computing and DevOps': [
+                'cloud', 'devops', 'aws', 'azure', 'gcp', 'ci/cd', 
+                'infrastructure as code', 'automation', 'monitoring'
+            ],
+            'Data Engineering and Analytics': [
+                'data engineering', 'data analytics', 'big data', 'data lakes', 
+                'data warehousing', 'etl', 'data pipelines', 'queries', 'SQL'
+            ],
+            'Security and Privacy': [
+                'security', 'privacy', 'compliance', 'gdpr', 'data protection',
+                'encryption', 'authentication', 'vulnerability', 'cybersecurity', 
+                'data protection', 'compliance', 'threat detection', 'incident response'
+            ]
+        }
